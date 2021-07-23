103944400bc023be30b23b86393d8ed01fcfcfe2
Thiago Puntar <thiagopuntar@gmail.com>
2021-07-22 22:07:26 -0300

    lint fixes

--- src/controllers/AgencyController.js
+++ src/controllers/AgencyController.js
@@ -1,24 +1,25 @@
-const ExistentAgencyError = require('../helpers/errors/ExistentAgencyError')
-const InvalidDataError = require('../helpers/errors/InvalidDataError')
+const ExistentAgencyError = require('../helpers/errors/ExistentAgencyError');
+const InvalidDataError = require('../helpers/errors/InvalidDataError');
 
 class AgencyController {
   constructor(agencyService) {
-    this.agencyService = agencyService
+    this.agencyService = agencyService;
   }
 
-  async create(req, res) {
-    const invalidData = this.agencyService.validate(req.body)
+  async create(req) {
+    const invalidData = this.agencyService.validate(req.body);
     if (invalidData) {
-      throw new InvalidDataError(invalidData)
+      throw new InvalidDataError(invalidData);
     }
 
-    const [existentAgency] = await this.agencyService.findAgencyByLicense(req.body.license)
+    const { license } = req.body;
+    const [existentAgency] = await this.agencyService.findAgencyByLicense(license);
     if (existentAgency.status === 'ACTIVE' && existentAgency.isFromTokio) {
-      throw new ExistentAgencyError(req.body.license)
+      throw new ExistentAgencyError(license);
     }
 
-    return { status: 400 }
+    return { status: 400 };
   }
 }
 
-module.exports = AgencyController
+module.exports = AgencyController;
