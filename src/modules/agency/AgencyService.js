a118afef7d2974c535fafee0b4cd9cbb6911ed25
Thiago Puntar <thiagopuntar@gmail.com>
2021-07-24 15:00:48 -0300

    initial service

--- src/modules/agency/AgencyService.js
+++ src/modules/agency/AgencyService.js
@@ -1,10 +1,37 @@
 module.exports = class AgencyService {
-  validate() {
-    return this;
+  constructor(agencyRepository) {
+    this.agencyRepository = agencyRepository;
   }
 
-  async findAgencyByLicense() {
-    return this;
+  // eslint-disable-next-line class-methods-use-this
+  validate(body) {
+    const { license, name } = body;
+    if (!license) {
+      return 'license is required.';
+    }
+
+    if (!name) {
+      return 'name is required.';
+    }
+
+    return null;
+  }
+
+  async findAgencyByLicense(license) {
+    const agency = await this.agencyRepository.findByLicense(license);
+    if (!agency) {
+      return null;
+    }
+
+    if (agency.status === 'ACTIVE' && agency.isFromTokio) {
+      return agency;
+    }
+
+    if (agency.status === 'PENDING' && agency.isFromTokio && agency.broker.status) {
+      return agency;
+    }
+
+    return null;
   }
 
   async createAgencyAtBroker() {
--- src/modules/agency/AgencyService.test.js
+++ src/modules/agency/AgencyService.test.js
@@ -1,11 +1,16 @@
+const faker = require('faker');
 const AgencyService = require('./AgencyService');
 
 describe('AgencyService', () => {
   const makeSut = () => {
-    const sut = new AgencyService();
+    const agencyRepositorySpy = {
+      findByLicense: jest.fn().mockResolvedValue(null),
+    };
+    const sut = new AgencyService(agencyRepositorySpy);
 
     return {
       sut,
+      agencyRepositorySpy,
     };
   };
 
@@ -25,6 +30,72 @@ describe('AgencyService', () => {
     it('should have method saveAgency defined', () => {
       const { sut } = makeSut();
       expect(sut.saveAgency).toBeDefined();
+    });
+  });
+  describe('validate', () => {
+    it('should return null when all fields are valid', () => {
+      const { sut } = makeSut();
+
+      const body = {
+        license: faker.random.word(),
+        name: faker.random.word(),
+      };
+      const result = sut.validate(body);
+      expect(result).toBe(null);
+    });
+    it('should return required when license is falsy', () => {
+      const { sut } = makeSut();
+
+      const body = {};
+      const result = sut.validate(body);
+      expect(result).toMatch('required');
+    });
+
+    it('should return required when name is falsy', () => {
+      const { sut } = makeSut();
+
+      const body = {
+        license: faker.random.word(),
+        name: '',
+      };
+      const result = sut.validate(body);
+      expect(result).toMatch('required');
+    });
+  });
+  describe('findAgencyByLicense', () => {
+    it('should call repository.findByLicence', async () => {
+      const { sut, agencyRepositorySpy } = makeSut();
+      const license = faker.random.word();
+      await sut.findAgencyByLicense(license);
+      expect(agencyRepositorySpy.findByLicense).toHaveBeenCalledWith(license);
+    });
+    it('should return null when no agency is found', async () => {
+      const { sut } = makeSut();
+      const result = await sut.findAgencyByLicense();
+      expect(result).toBe(null);
+    });
+    it('should return found agency when status ACTIVE & isFromTokio', async () => {
+      const { sut, agencyRepositorySpy } = makeSut();
+      const agency = {
+        status: 'ACTIVE',
+        isFromTokio: true,
+      };
+      agencyRepositorySpy.findByLicense.mockResolvedValue(agency);
+      const result = await sut.findAgencyByLicense();
+      expect(result).toBe(agency);
+    });
+    it('should return found agency when status PENDING, isFromTokio & broker.status is truthy', async () => {
+      const { sut, agencyRepositorySpy } = makeSut();
+      const agency = {
+        status: 'PENDING',
+        isFromTokio: true,
+        broker: {
+          status: faker.random.word(),
+        },
+      };
+      agencyRepositorySpy.findByLicense.mockResolvedValue(agency);
+      const result = await sut.findAgencyByLicense();
+      expect(result).toBe(agency);
     });
   });
 });
