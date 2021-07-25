7ac50afa2d64422c67ecb906419c47ef46975d48
Thiago Puntar <thiagopuntar@gmail.com>
2021-07-25 19:49:25 -0300

    setup swagger

--- src/main/index.js
+++ src/main/index.js
@@ -1,10 +1,14 @@
 /* eslint-disable no-console */
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
 const router = require('./routes');
 
+const swaggerDocument = YAML.load('./swagger.yml');
 const app = express();
 app.use(express.json());
 app.use(router);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
